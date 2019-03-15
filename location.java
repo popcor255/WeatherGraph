f189de605e203730dbccd2bbb3ea76e07d105732
Napoleon Santana <popcor255@gmail.com>
2019-03-15 16:30:28 -0700

    saving progress..

--- location.java
+++ location.java
@@ -2,14 +2,14 @@ public class location{
 
     public String name;
     public String weather;
-    public int temp;
-    public int dew_point;
-    public int rel_humidity;
+    public String temp;
+    public String dew_point;
+    public String rel_humidity;
     public String wind;
     public String pressure;
 
     public location(){
-        
+
     }
 
 }
--- weather.java
+++ weather.java
@@ -5,15 +5,27 @@ public class weather{
     public static void main(String[] args){
         textProccessor tp = new textProccessor("weather_report.txt");
         String lines[] = tp.output.split("[\\r\\n]+");
+        ArrayList<location> l = new ArrayList<location>;
 
-        System.out.println(Arrays.toString(lines));
-        /*
         for(int i = 0; i < lines.length ; i++){
-            System.out.println("location => " + lines[i].substring(0,14).replaceAll(" ", ""));
-            System.out.println("weather => " + lines[i].substring(0,23).replaceAll(" ", ""));
+            location temp_loc = new location();
+            temp_loc.name = getSubString(0, 14, lines[i]);
+            temp_loc.weather = getSubString(14, 24, lines[i]);
+            temp_loc.loc = getSubString(25, 27, lines[i]);
 
-        }*/
+            System.out.println("location => " + getSubString(0, 14, lines[i]));
+            System.out.println("weather => " + getSubString(14, 24, lines[i]));
+            System.out.println("temp => " + getSubString(25, 27, lines[i]));
+            System.out.println("dew_point => " + getSubString(29, 31, lines[i]));
+            System.out.println("rel_humidity => " + getSubString(33, 35, lines[i]));
+            System.out.println("wind => " + getSubString(36, 39, lines[i]));
+            System.out.println("pressure => " + getSubString(46, 51, lines[i]));
+        }
 
+    }
+
+    public static String getSubString(int start, int end, String in){
+        return in.substring(start,end).replaceAll(" ", "");
     }
 
 }
